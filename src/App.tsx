662e336a55295d5f3545447ed109d03bcf084c8b
Aaron Wu <awu0626@seas.upenn.edu>
2025-06-16 23:04:37 -0400

    added 404.html

--- src/App.tsx
+++ src/App.tsx
@@ -13,45 +13,9 @@ import { FaCoffee } from "react-icons/fa";
 
 const { Sider, Content, Header } = Layout;
 
-// add stripe payment provider
-// import {CheckoutProvider} from '@stripe/react-stripe-js';
-// import {loadStripe} from '@stripe/stripe-js';
-
-// const stripePromise = loadStripe('pk_test_51RZMrMQCihKGhrfJF5GMOHqvUwf6c9VrE8JhceWRe4XSaDk026F2aWWAhY7uEgHlWdF54yDDy4plijw7JuUb6WO300CD1nSPCM');
-// const fetchClientSecret = async () => {
-//     async function createStripeCheckoutSession() {
-//         try {
-//             const response = await fetch('https://pfsxss3wr9.execute-api.us-east-2.amazonaws.com/default/createStripeCheckoutSession', {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                     'Accept': 'application/json'
-//                 },
-    
-//                 body: JSON.stringify({ productId: 'coffee donation', quantity: 1 })
-//             });
-        
-//             if (!response.ok) {
-//                 const errorData = await response.json().catch(() => ({ error: 'Unknown server error' }));
-//                 throw new Error(errorData.error || `HTTP error! Status: ${response.status}`);
-//             }
-        
-//             const data = await response.json();
-//             console.log('Received from API Gateway:', data);
-//             return data;
-//         } catch (error) {
-//             console.error('Error during checkout process:', error);
-//         }
-//     }
-    
-//     const result = createStripeCheckoutSession();
-//     return result;
-// };
-
 function App() {
     const [dimensions, setDimensions] = useState<{
-        height: number,
-        width: number
+        height: number, width: number
     }>({
         height: window.innerHeight,
         width: window.innerWidth
@@ -66,7 +30,7 @@ function App() {
             case '/anime': return 'Anime Collection';
             case '/projects': return 'My Projects';
             case '/contact': return 'My Contact';
-            case '/donate': return 'Fund my Dream'
+            case '/donate': return 'Fund My Dream'
             default: return 'Welcome';
         }
     };
@@ -104,7 +68,7 @@ function App() {
         { key: 'anime', icon: <SiMyanimelist size={18} />, label: <Link to="/anime">Anime</Link> },
         { key: 'projects', icon: <IoMdCode size={18} />, label: <Link to="/projects">Projects</Link> },
         { key: 'contact', icon: <MdPersonAddAlt1 size={18} />, label: <Link to="/contact">Contact</Link> },
-        { key: 'donate', icon: <FaCoffee size={18} />, label: <Link to="/donate">Donate</Link> },
+        { key: 'donate', icon: <FaCoffee size={18} />, label: <Link to="/donate">Support me!</Link> },
     ];
 
     const socialIcons = [
@@ -125,7 +89,6 @@ function App() {
     ];
 
     return (
-        // <CheckoutProvider stripe={stripePromise} options={{fetchClientSecret}}>
             <Layout style={{ 
                 minHeight: '100vh',
                 width: '100vw',
@@ -223,7 +186,6 @@ function App() {
                     </Layout.Footer>
                 </Layout>
             </Layout>
-        // {/* </CheckoutProvider> */}
     );
 }
 
