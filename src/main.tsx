ab6d64c7bb055d0071aa9882aa52b76cc29c1326
Aaron Wu <awu0626@seas.upenn.edu>
2025-06-16 23:13:43 -0400

    changed browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router'
 
 import App from './App'
 import Home from './pages/Home'
@@ -9,7 +9,7 @@ import Projects from './pages/Projects'
 import Donate from './pages/Donate'
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/' element={<App />}>
                 <Route index element={<Home />} />
@@ -19,5 +19,5 @@ createRoot(document.getElementById('root')!).render(
                 <Route path='donate' element={<Donate />} />
             </Route>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 )
