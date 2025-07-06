79917b1d8c0f9bc2a0b539e42fcdb31ee80ab539
Aaron Wu <awu0626@seas.upenn.edu>
2025-07-06 15:14:59 -0400

    resolved footer pushed away issue on anime page

--- server/anime-api/getTopAnime/index.js
+++ server/anime-api/getTopAnime/index.js
@@ -20,9 +20,6 @@ exports.handler = async (event) => {
         return {
             statusCode: 200,
             headers: {
-                "Access-Control-Allow-Origin": "http://localhost:5173",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
